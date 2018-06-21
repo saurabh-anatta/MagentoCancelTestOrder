fd7c4ad4daa2cfbc327162fa40f4446531d60bc2
saurabh chaudhary <saurabh@anattadesign.com>
2018-06-22 00:25:25 +0530

    added missing , in explode function

--- app/code/local/AnattaDesign/CancelTestOrder/Model/Observer.php
+++ app/code/local/AnattaDesign/CancelTestOrder/Model/Observer.php
@@ -9,7 +9,7 @@ class AnattaDesign_CancelTestOrder_Model_Observer {
 	 */
 	public function changeOrderStatusToCancel($observer) {
 		$orderIds = $observer->getEvent()->getOrderIds();
-		$testCouponCode = explode('', Mage::getStoreConfig('sales/testorder/coupon_codes'));
+		$testCouponCode = explode(',', Mage::getStoreConfig('sales/testorder/coupon_codes'));
 
 		$orders = Mage::getModel('sales/order')->getCollection()->addFieldToFilter('entity_id', array('in'=> $orderIds));
 		foreach ($orders as $order) {
